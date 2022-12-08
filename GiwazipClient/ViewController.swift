6488e61a54b91decb296138419d1c91ebb4ecfd0
Juny0110 <wnsdyds403@naver.com>
2022-12-08 18:01:31 +0900

    Add Init ProjectFile
    - 초기 파일 생성

---
+++ GiwazipClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GiwazipClient
+//
+//  Created by 지준용 on 2022/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
