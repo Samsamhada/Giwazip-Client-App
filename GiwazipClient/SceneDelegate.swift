014c92e2db4232e7cf12aa703625b5d0c620e416
Juny0110 <wnsdyds403@naver.com>
2023-01-19 02:04:23 +0900

    Style:art:: #11 Delete unnecessary function

--- GiwazipClient/SceneDelegate.swift
+++ GiwazipClient/SceneDelegate.swift
@@ -23,19 +23,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window?.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
 }
