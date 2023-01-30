52e981b816f4df5266f815672b71c10918648f92
Juny0110 <wnsdyds403@naver.com>
2023-01-31 01:24:15 +0900

    Feat:sparkles:: #30 Add unselected thumbnail Blur

--- GiwazipClient/Cells/PostCell.swift
+++ GiwazipClient/Cells/PostCell.swift
@@ -12,22 +12,22 @@ import SnapKit
 class PostCell: UICollectionViewCell {
 
     // MARK: - Property
-    
+
     static let identifier = "PostCell"
-    
+
     // MARK: - View
-    
+
     let postingImage: UIImageView = {
         $0.image = UIImage(named: "Test01")
         $0.contentMode = .scaleToFill
         return $0
     }(UIImageView())
-    
+
     let whiteBlock: UIView = {
         $0.backgroundColor = UIColor(white: 1, alpha: 0.5)
         return $0
     }(UIView())
-    
+
     let strokeBlock: UIView = {
         $0.layer.borderWidth = 3
         $0.layer.borderColor = UIColor.black.cgColor
@@ -35,30 +35,30 @@ class PostCell: UICollectionViewCell {
     }(UIView())
 
     // MARK: - LifeCycle
-    
+
     override private init(frame: CGRect) {
         super.init(frame: frame)
         
         setupCell()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     // MARK: - Method
-    
+
     private func setupCell() {
         addSubview(postingImage)
         postingImage.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
-        
+
         postingImage.addSubview(whiteBlock)
         whiteBlock.snp.makeConstraints{
             $0.edges.equalToSuperview()
         }
-        
+
         addSubview(strokeBlock)
         strokeBlock.snp.makeConstraints {
             $0.edges.equalToSuperview()
