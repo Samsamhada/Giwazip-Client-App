904a7dbf77235ff3a2cfe1e19b08a5fe854e1a49
Dinner <junghwan3588@gm.gist.ac.kr>
2023-02-02 08:53:17 +0900

    Style:art:: #28 Modify Code

--- GiwazipClient/Cells/ASCell.swift
+++ GiwazipClient/Cells/ASCell.swift
@@ -16,7 +16,7 @@ class ASCell: UICollectionReusableView {
     static let identifier = "ASCell"
     
     private let remainedDuration: UILabel = {
-        $0.text = "AS 기간(~23.11.12)"
+        $0.text = "AS 기간 (~23.11.12)"
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
         $0.textColor = .gray
@@ -25,7 +25,7 @@ class ASCell: UICollectionReusableView {
     
     private let zStack: UIView = UIView()
     
-    let remainedDay: UILabel = {
+    private let remainedDay: UILabel = {
         $0.text = "102일"
         $0.textAlignment = .center
         $0.font = UIFont.systemFont(ofSize: 22, weight: .bold)
@@ -43,39 +43,37 @@ class ASCell: UICollectionReusableView {
     
     // MARK: - LifeCycle
     
-    override init(frame: CGRect) {
+    override private init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .lightGray
-        layout()
+        setupCell()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:)가 실행되지 않았습니다.")
     }
     
-    private func layout() {
+    private func setupCell() {
         addSubview(remainedDuration)
         remainedDuration.snp.makeConstraints {
-            $0.top.left.equalToSuperview().inset(15)
+            $0.top.left.equalToSuperview().inset(16)
         }
         
         addSubview(zStack)
         zStack.snp.makeConstraints {
-            $0.top.equalTo(remainedDuration.snp.bottom).offset(15)
+            $0.top.equalTo(remainedDuration.snp.bottom).offset(16)
             $0.centerX.equalToSuperview()
         }
         
         zStack.addSubview(remainedDay)
         remainedDay.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
-            $0.left.equalToSuperview()
+            $0.top.left.bottom.equalToSuperview()
         }
         
         zStack.addSubview(remainedText)
         remainedText.snp.makeConstraints {
-            $0.top.bottom.equalToSuperview()
+            $0.top.bottom.right.equalToSuperview()
             $0.left.equalTo(remainedDay.snp.right).offset(4)
-            $0.right.equalToSuperview()
         }
     }
 }
