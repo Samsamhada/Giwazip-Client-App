cca8d693b922f78f35bc838c8369758c7ebbd87e
Juny0110 <wnsdyds403@naver.com>
2023-02-19 23:19:40 +0900

    Feat:sparkles:: #36 scene delay

--- GiwazipClient/SceneDelegate.swift
+++ GiwazipClient/SceneDelegate.swift
@@ -10,17 +10,27 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: scene)
 
-        let navigationController = UINavigationController()
-        self.window?.rootViewController = navigationController
-
-        let coordinator = AppCoordinator(navigationController: navigationController)
-        coordinator.start()
-
+        window?.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
         window?.makeKeyAndVisible()
+        
+        NetworkManager.shared
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
+            guard let scene = (scene as? UIWindowScene) else { return }
+            self.window = UIWindow(windowScene: scene)
+
+            let navigationController = UINavigationController()
+            self.window?.rootViewController = navigationController
+
+            let coordinator = AppCoordinator(navigationController: navigationController)
+            coordinator.start()
+
+            self.window?.makeKeyAndVisible()
+        }
+
+        guard let _ = (scene as? UIWindowScene) else { return }
     }
 }
