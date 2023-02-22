176a893714dd9510702eebca36857bad63fe17e5
taek0622 <min4644@gmail.com>
2023-02-23 00:14:42 +0900

    Chore:wrench:: #51 Modify Text
    - 고객센터 및 알러트 텍스트 변경

--- GiwazipClient/Literals/TextLiteral.swift
+++ GiwazipClient/Literals/TextLiteral.swift
@@ -63,14 +63,14 @@ enum TextLiteral {
     // MARK: - SettingView
 
     static let settingItems = [["고객 정보 수정"],
-                               ["공지사항", "이용약관", "개인정보 처리방침", "개발자 정보", "오픈소스 라이브러리", "고객센터", "버전 정보"],
+                               ["공지사항", "이용약관", "개인정보 처리방침", "개발자 정보", "오픈소스 라이센스", "개발자에게 문의하기", "버전 정보"],
                                ["시공 마감하기"]]
 
     static let termsConditionURL = "https://jitda.notion.site/56f7a03bacc145bc8249b1cb22bed2a9"
     static let privacyPolicyURL = "https://jitda.notion.site/1627d263673e499490ae116cc049f9d3"
 
-    static let customerServiceErrorTitle = "고객센터를 이용할 수 없습니다"
-    static let customerServiceErrorMessage = "고객센터는 메일 앱이 사용 가능한 상태에만 이용할 수 있습니다.\n아이폰 기본 메일 앱이 사용 가능한 상태인지 확인해주세요."
+    static let customerServiceErrorTitle = "해당 기능을 이용할 수 없습니다"
+    static let customerServiceErrorMessage = "'개발자에게 문의하기'는 메일 앱이\n사용 가능한 상태에만 이용할 수 있습니다.\n아이폰 기본 메일 앱이\n사용 가능한 상태인지 확인해주세요."
 
     static let customerServiceEmail = ["samsamhada0915@gmail.com"]
     static let customerServiceMailSubject = "[문의 사항]"
