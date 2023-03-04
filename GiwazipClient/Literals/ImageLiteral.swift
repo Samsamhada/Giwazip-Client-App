ab62e942f0844955d86f37f89cb63c53f4f6249b
taek0622 <min4644@gmail.com>
2023-03-05 05:45:46 +0900

    Chore:wrench:: #51 Add ImageLiteral
    - 이미지 리터럴 파일 추가

---
+++ GiwazipClient/Literals/ImageLiteral.swift
@@ -0,0 +1,12 @@
+//
+//  ImageLiteral.swift
+//  GiwazipClient
+//
+//  Created by 김민택 on 2023/03/05.
+//
+
+import UIKit
+
+enum ImageLiteral {
+    static let githubLogo = UIImage(named: "GitHubLogo")
+}
