388a31fde3abf65b33c706ba8bb93f40e394add4
Juny0110 <wnsdyds403@naver.com>
2023-03-16 00:44:53 +0900

    Chore:wrench:: #66 Add DeveloperCoordinator file

---
+++ GiwazipClient/Coordinators/DeveloperCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  DeveloperCoordinator.swift
+//  GiwazipClient
+//
+//  Created by 지준용 on 2023/03/14.
+//
+
+class DeveloperCoordinator: BaseCoordinator, SettingContentDelegate {
+
+    var delegate: SettingContentCoordinatorDelegate?
+    
+    override func start() {
+        let developerViewController = DeveloperViewController()
+        developerViewController.delegate = self
+
+        navigationController.pushViewController(developerViewController, animated: true)
+    }
+    
+    func popToSettingView() {
+        delegate?.popToSettingView()
+    }
+}
