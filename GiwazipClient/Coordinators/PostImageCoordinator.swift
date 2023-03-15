667bc7e2fc753b2d94d263caf48700c7e5b5c81e
Juny0110 <wnsdyds403@naver.com>
2023-03-16 00:47:43 +0900

    Chore:wrench:: #66 Add PostImageCoordinator file

---
+++ GiwazipClient/Coordinators/PostImageCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  PostImageCoordinaor.swift
+//  GiwazipClient
+//
+//  Created by 지준용 on 2023/03/11.
+//
+
+protocol PostImageCoordinatorDelegate {
+    func popToPostView()
+}
+
+class PostImageCoordinator: BaseCoordinator, PostImageViewControllerDelegate {
+
+    var delegate: PostImageCoordinatorDelegate?
+    
+    override func start() {
+        let postImageViewController = PostImageViewController()
+        postImageViewController.delegate = self
+
+        navigationController.pushViewController(postImageViewController, animated: true)
+    }
+    
+    func popToPostView() {
+        delegate?.popToPostView()
+    }
+}
