18c048643f0b605f7644321e245546f46d8fac87
Juny0110 <wnsdyds403@naver.com>
2023-03-16 00:48:12 +0900

    Chore:wrench:: #66 Add PostingCoordinator file

---
+++ GiwazipClient/Coordinators/PostingCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  PostingPhotoCoordinator.swift
+//  GiwazipClient
+//
+//  Created by 지준용 on 2023/02/02.
+//
+
+import UIKit
+
+import UIKit
+
+protocol PostingPhotoCoordinatorDelegate {
+    func dismissPostingView()
+    func popToPostingPhotoView()
+}
+
+class PostingCoordinator: BaseCoordinator, PostingPhotoViewControllerDelegate, PostingTextViewControllerDelegate {
+    
+    var delegate: PostingPhotoCoordinatorDelegate?
+    
+    override func start() {
+        let postingPhotoViewController = PostingPhotoViewController()
+        postingPhotoViewController.delegate = self
+
+        rootViewController = UINavigationController(rootViewController: postingPhotoViewController)
+        rootViewController.modalPresentationStyle = .fullScreen
+
+        navigationController.present(rootViewController, animated: true)
+    }
+
+    func presentPostingTextView() {
+        let postingTextViewController = PostingTextViewController()
+        postingTextViewController.delegate = self
+
+        rootViewController.pushViewController(postingTextViewController, animated: true)
+    }
+    
+    func popToPostingPhotoView() {
+        childCoordinators.removeLast()
+        rootViewController.popViewController(animated: true)
+    }
+    
+    func dismissPostingView() {
+        delegate?.dismissPostingView()
+    }
+}
