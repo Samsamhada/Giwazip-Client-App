7424066256c7a8a9252e6e69a26c02b3c281a769
Juny0110 <wnsdyds403@naver.com>
2023-03-16 01:41:42 +0900

    Chore:wrench:: #66 Add presentEditingView

--- GiwazipClient/Coordinators/PostCoordinator.swift
+++ GiwazipClient/Coordinators/PostCoordinator.swift
@@ -7,19 +7,25 @@
 
 protocol PostCoordinatorDelegate {
     func presentPostImageView()
+    func presentEditingView()
 }
 
 class PostCoordinator: BaseCoordinator, PostViewControllerDelegate {
 
-    let postViewController = PostViewController()
-
     var delegate: PostCoordinatorDelegate?
 
     override func start() {
+        let postViewController = PostViewController()
         postViewController.delegate = self
+        
+        navigationController.pushViewController(postViewController, animated: true)
     }
 
     func presentPostImageView() {
         delegate?.presentPostImageView()
+    }
+    
+    func presentEditingView() {
+        delegate?.presentEditingView()
     }
 }
