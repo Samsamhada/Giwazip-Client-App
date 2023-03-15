50d7ae8252e78edd065be2a8cdff30d619919959
Juny0110 <wnsdyds403@naver.com>
2023-03-16 01:01:44 +0900

    Chore:wrench:: #66 Apply SplashView Coordinator

--- GiwazipClient/Views/SplashViewController.swift
+++ GiwazipClient/Views/SplashViewController.swift
@@ -9,16 +9,23 @@ import UIKit
 
 import SnapKit
 
+protocol SplashViewControllerDelegate {
+    func presentEnterView()
+}
+
 class SplashViewController: BaseViewController {
 
     // MARK: - Property
 
+    var delegate: SplashViewControllerDelegate?
+    
     private let enterButton: UIButton = {
         $0.setTitle(TextLiteral.enterButtonText, for: .normal)
         $0.setTitleColor(.black, for: .normal)
         $0.titleLabel?.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
         $0.backgroundColor = .gray
         $0.layer.cornerRadius = 16
+        $0.addTarget(self, action: #selector(didTapEnterButton), for: .touchUpInside)
         return $0
     }(UIButton())
 
@@ -32,5 +39,9 @@ class SplashViewController: BaseViewController {
             $0.left.right.bottom.equalTo(view.safeAreaLayoutGuide).inset(16)
             $0.height.equalTo(50)
         }
+    }
+    
+    @objc func didTapEnterButton() {
+        delegate?.presentEnterView()
     }
 }
