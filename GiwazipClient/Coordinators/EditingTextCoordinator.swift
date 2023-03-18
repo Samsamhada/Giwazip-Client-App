cc8522f565f3a9f1063dfe384f66a930d19bbcec
Juny0110 <wnsdyds403@naver.com>
2023-03-18 21:17:16 +0900

    Chore:wrench:: #66 Add EditingTextCoordinator

---
+++ GiwazipClient/Coordinators/EditingTextCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  PostingTextView.swift
+//  GiwazipClient
+//
+//  Created by 지준용 on 2023/03/16.
+//
+
+import Foundation
+import UIKit
+
+protocol EditingTextCoordinatorDelegate {
+    func dismissPostingTextView()
+    func dismissEditingTextView()
+}
+
+class EditingTextCoordinator: BaseCoordinator, EditingTextViewControllerDelegate {
+
+    var delegate: EditingTextCoordinatorDelegate?
+
+    override func start() {
+        let postingTextViewController = PostingTextViewController()
+        let postViewController = PostViewController()
+        
+        postingTextViewController.delegate = self
+        
+        postingTextViewController.textView.text = postViewController.postingDescription.text
+        postingTextViewController.isEditView = true
+//
+        rootViewController = UINavigationController(rootViewController: postingTextViewController)
+        rootViewController.modalPresentationStyle = .fullScreen
+
+        self.navigationController.present(rootViewController, animated: true)
+    }
+    
+    func dismissPostingTextView() {
+        delegate?.dismissPostingTextView()
+    }
+    
+    func dismissEditingTextView() {
+        delegate?.dismissEditingTextView()
+    }
+}
